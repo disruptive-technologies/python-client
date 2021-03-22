7276f299c8e780ba04dd131bd2f4dd393ee79aed
Johannes H.Gjeraker <johannes@disruptive-technologies.com>
2021-03-22 15:37:45 +0100

    docstrings

--- disruptive/authentication.py
+++ disruptive/authentication.py
@@ -14,12 +14,42 @@ import disruptive.transforms as dttrans
 
 
 class Auth():
+    """
+    Parent class for authenticating the REST API.
+
+    Attributes
+    ----------
+    token : str
+        Access token added to the request header.
+
+    """
 
     def __init__(self) -> None:
-        # Initialise variables
+        """
+        Constructs the Auth object by initializing token to empty string.
+
+        Parameters
+        ----------
+        project : dict
+            Unmodified project response dictionary.
+
+        """
+
+        # Initialise attributes.
         self.token = ''
 
     def get_token(self) -> str:
+        """
+        Returns the access token.
+        If the token has expired, renew it.
+
+        Returns
+        -------
+        token : str
+            Access token added to the request header.
+
+        """
+
         # Check expiration time.
         if self.has_expired():
             # Renew access token.
@@ -28,12 +58,20 @@ class Auth():
         return self.token
 
     def refresh(self) -> None:
-        # This function only exists to please Mypy type-checking.
-        # If unauthorized, it should never be called.
-        # Once authorized, it will be overwritten by the child class.
+        """
+        This function only exists as a placeholder for when unauthenticated
+        and should never be called until authentication has been set up.
+        Once authenticated it will be overwritten by the child class.
+        """
         pass
 
     def has_expired(self) -> bool:
+        """
+        Raises an Unauthenticated error with some added information.
+        If this function is called, the project has not yet been
+        authenticated, and the user should be reminded to do so.
+
+        """
         raise dterrors.Unauthenticated(
             'Authentication object not initialized.\n\n\
             Set globally by calling one of the following methods:\n\
@@ -42,6 +80,20 @@ class Auth():
         )
 
     def _verify_str_credentials(self, credentials: list[str]) -> None:
+        """
+        Verifies that the provided credentials are strings.
+
+        This check is added as people use environment variables, but
+        if for instance os.environ.get() does not find one, it silently
+        returns None. It's better to just check for it early.
+
+        Parameters
+        ----------
+        credentials : list[str]
+            List of credentials used to initialize the authentication routine.
+
+        """
+
         for c in credentials:
             if type(c) != str:
                 raise TypeError(
@@ -52,8 +104,34 @@ class Auth():
 
 
 class BasicAuth(Auth):
+    """
+    Class for authenticating the REST API using Basic Auth scheme.
+
+    Attributes
+    ----------
+    token : str
+        Access token added to the request header.
+    key_id : str
+        Service Account Key ID.
+    secret : str
+        Service Account secret.
+
+    """
 
     def __init__(self, key_id: str, secret: str) -> None:
+        """
+        Constructs the BasicAuth object by inheriting parent, then
+        constructing the token from the provided credentials.
+
+        Parameters
+        ----------
+        key_id : str
+            Service Account Key ID.
+        secret : str
+            Service Account secret.
+
+        """
+
         # Inherit everything from parent.
         super().__init__()
 
@@ -71,12 +149,54 @@ class BasicAuth(Auth):
         )
 
     def has_expired(self) -> bool:
+        """
+        Returns False as Basic Auth token is static
+        and does not need to be refreshed.
+
+        Returns
+        -------
+        false : bool
+            Always returns False.
+
+        """
         return False
 
 
 class OAuth(Auth):
+    """
+    Class for authenticating the REST API using an OAuth2 scheme.
+
+    Attributes
+    ----------
+    token : str
+        Access token added to the request header.
+    key_id : str
+        Service Account Key ID.
+    secret : str
+        Service Account secret.
+    email : str
+        Service Account email address.
+    expiration : int
+        Unixtime of when the access token expires.
+
+    """
 
     def __init__(self, key_id: str, secret: str, email: str) -> None:
+        """
+        Constructs the OAuth object by inheriting parent, then
+        initializing attributes needed for refresh scheme later.
+
+        Parameters
+        ----------
+        key_id : str
+            Service Account Key ID.
+        secret : str
+            Service Account secret.
+        email : str
+            Service Account email address.
+
+        """
+
         # Inherit everything from parent.
         super().__init__()
 
@@ -90,6 +210,16 @@ class OAuth(Auth):
         self.expiration = 0
 
     def __get_access_token(self) -> dict:
+        """
+        Constructs and exchanges the JWT for an access token.
+
+        Returns
+        -------
+        response : dict
+            Dictionary containing expiration and the token itself.
+
+        """
+
         # Set access token URL.
         token_url = 'https://identity.disruptive-technologies.com/oauth2/token'
 
@@ -142,12 +272,30 @@ class OAuth(Auth):
         return access_token_response
 
     def has_expired(self) -> bool:
+        """
+        Evaluates whether the access token has expired.
+
+        Returns
+        -------
+        has_expired : bool
+            True if the access token has expired, otherwise False.
+
+        """
+
         if time.time() > self.expiration:
             return True
         else:
             return False
 
     def refresh(self) -> None:
+        """
+        Refreshes the access token.
+
+        This first exchanges the JWT for an access token, then updates
+        the expiration and token attributes with the response.
+
+        """
+
         response = self.__get_access_token()
         self.expiration = time.time() + response['expires_in']
         self.token = 'Bearer {}'.format(response['access_token'])
