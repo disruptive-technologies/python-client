e7ad7e958f79e446cb4a987da2f4577449f598eb
Johannes H.Gjeraker <johannes@disruptive-technologies.com>
2021-03-22 15:46:09 +0100

    docstrings

--- disruptive/resources/event_history.py
+++ disruptive/resources/event_history.py
@@ -11,6 +11,11 @@ from disruptive.authentication import BasicAuth, OAuth
 
 
 class EventHistory():
+    """
+    Contains staticmethods for fetching event history.
+    Used for namespacing only and thus does not have a constructor
+
+    """
 
     @staticmethod
     def list_events(project_id: str,
@@ -20,6 +25,33 @@ class EventHistory():
                     end_time: Optional[str] = None,
                     auth: Optional[BasicAuth | OAuth] = None,
                     ) -> list[Event]:
+        """
+        Get the event history for a single device.
+
+        Parameters
+        ----------
+        project_id : str
+            Unique ID of the target project.
+        device_id : str
+            Unique ID of the target device.
+        event_types : list[str], optional
+            If provided, only the specified event types are fetched.
+        start_time : str, optional
+            Specifies from when event history is fetched.
+            Defaults to 24 hours ago.
+        end_time : str, optional
+            Specified until when event history is fetched.
+            Defaults to now.
+        auth: BasicAuth, OAuth, optional
+            Authorization object used to authenticate the REST API.
+            If provided it will be prioritized over global authentication.
+
+        Returns
+        -------
+        events : list[Event]
+            List of objects each representing an event in fetched history.
+
+        """
 
         # Construct URL.
         url = dt.base_url
