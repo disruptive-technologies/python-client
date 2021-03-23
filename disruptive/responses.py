291c6789484911ec55b8b052f1a4714b6010a71d
Johannes H.Gjeraker <johannes@disruptive-technologies.com>
2021-03-23 15:08:55 +0100

    docstrings

--- disruptive/responses.py
+++ disruptive/responses.py
@@ -1,6 +1,19 @@
 
 
 class DTResponse():
+    """
+    Represents the REST API responses.
+
+    Attributes
+    ----------
+    data : dict
+        Raw response data dictionary.
+    status_code : int
+        Response status code.
+    headers : dict
+        Headers in the response.
+
+    """
 
     def __init__(self, data: dict, status_code: int, headers: dict) -> None:
         self.data = data
