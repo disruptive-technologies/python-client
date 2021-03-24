a014c43d9668e5ef2af6ca668d1d21539656a6b5
Johannes H.Gjeraker <johannes@disruptive-technologies.com>
2021-03-24 17:19:22 +0100

    renamed

--- examples/get_device.py
+++ examples/get_device.py
@@ -1,15 +1,21 @@
+# Standard library imports.
 import os
+
+# Import disruptive package.
 import disruptive as dt
 
-dt.Auth.serviceaccount(
+# Authenticate the package using serviceaccount credentials.
+dt.auth = dt.Auth.serviceaccount(
     key_id=os.environ.get('DT_SERVICE_ACCOUNT_KEY_ID', ''),
     secret=os.environ.get('DT_SERVICE_ACCOUNT_SECRET', ''),
     email=os.environ.get('DT_SERVICE_ACCOUNT_EMAIL', ''),
 )
 
+# Get the device of interest.
 device = dt.Device.get_device(
     project_id=os.environ.get('DT_PROJECT_ID', ''),
     device_id=os.environ.get('DT_DEVICE_ID', ''),
 )
 
-print(device.id)
+# Print the device information to console.
+print(device)
