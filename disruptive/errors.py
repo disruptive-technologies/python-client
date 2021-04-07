d9e281aeac660bdb64ceb5699500fcc7f705efb0
Johannes H.Gjeraker <johannes@disruptive-technologies.com>
2021-04-07 14:15:25 +0200

    501 catch added

--- disruptive/errors.py
+++ disruptive/errors.py
@@ -107,6 +107,16 @@ class InternalServerError(DTApiError):
         super().__init__(message)
 
 
+class ServiceUnavailable(DTApiError):
+    """
+    The response contained a status code of 501.
+
+    """
+
+    def __init__(self, message):
+        super().__init__(message)
+
+
 class ReadTimeout(Exception):
     """
     The server did not send any data in the allotted amount of time.
@@ -120,6 +130,16 @@ class ReadTimeout(Exception):
 class ConnectionError(Exception):
     """
     Could not establish connection to the server.
+
+    """
+
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class FormatError(Exception):
+    """
+    Some formatting, usually timestamp strings, is wrong.
 
     """
 
@@ -189,7 +209,11 @@ def parse_error(res: DTResponse, retry_count: int):
                 return (TooManyRequests(res.data), False, None)
         elif res.status_code == 500:
             return (InternalServerError(res.data), True, retry_count**2)
+        elif res.status_code == 501:
+            return (ServiceUnavailable(res.data), False, None)
         elif res.status_code == 503:
             return (InternalServerError(res.data), True, retry_count**2)
         elif res.status_code == 504:
             return (InternalServerError(res.data), True, retry_count**2 + 9)
+        else:
+            return (Exception(res.data), False, None)
