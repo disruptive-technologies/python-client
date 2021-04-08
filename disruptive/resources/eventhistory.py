4a9352fecf2ba26d70aacb141dc89b269fbf450e
Johannes H.Gjeraker <johannes@disruptive-technologies.com>
2021-04-08 22:22:46 +0200

    convenience functions for fetching sorted events and axes

--- disruptive/resources/eventhistory.py
+++ disruptive/resources/eventhistory.py
@@ -106,324 +106,52 @@ class EventHistory(dtoutputs.OutputBase):
         # Return list of Event objects of paginated GET response.
         return cls(Event.from_mixed_list(res))
 
-    def get_touch_events(self):
+    def get_events(self, event_type):
         """
-        Returns a filtered list of touch events from history.
+        Returns a filtered list of events specified by type from history.
+
+        Parameters
+        ----------
+        event_type : str
+            The event type to be returned.
 
         Returns
         -------
-        events : list[Event]
-            List of touch events.
+        events : list
+            List of event objects of the specified type.
 
         """
-
-        return [e for e in self.events_list if e.event_type == 'touch']
-
-    def get_touch_axes(self):
-        """
-        Returns filtered list of timestamps from touch events in history.
-
-        Returns
-        -------
-        timestamp : list[datetime]
-            List of event timestamps.
-
-        """
-
-        x = []
+        out = []
         for e in self.events_list:
-            if e.event_type == 'touch':
-                x.append(e.data.timestamp)
-        return x
+            if e.event_type == event_type:
+                out.append(e)
+        return out
 
-    def get_temperature_events(self):
+    def get_axes(self, x, y=None, event_type=None):
         """
-        Returns a filtered list of temperature events from history.
+        Returns filtered lists of the data axes as specified from
+        event type in history.
 
-        Returns
-        -------
-        events : list[Event]
-            List of temperature events.
-
-        """
-
-        return [e for e in self.events_list if e.event_type == 'temperature']
-
-    def get_temperature_axes(self):
-        """
-        Returns filtered lists of timestamps and values
-        from temperature events in history.
-
-        Returns
-        -------
-        timestamp : list[datetime]
-            List of event timestamps.
-        temperature : list[float]
-            List of temperature values in degrees Celsius.
+        Parameters
+        ----------
+        x : str
+            Name of .
 
         """
 
-        x = []
-        y = []
+        x_axis = []
+        y_axis = []
         for e in self.events_list:
-            if e.event_type == 'temperature':
-                x.append(e.data.timestamp)
-                y.append(e.data.temperature)
-        return x, y
+            # Skip event if event_type mismatch.
+            if event_type is not None or event_type != e.event_type:
+                continue
 
-    def get_objectpresent_events(self):
-        """
-        Returns a filtered list of objectPresent events from history.
+            # Tests passed, append timestamp and y-axis.
+            x_axis.append(getattr(e.data, x))
+            if y is not None:
+                y_axis.append(getattr(e.data, y))
 
-        Returns
-        -------
-        events : list[Event]
-            List of objectPresent events.
-
-        """
-
-        return [e for e in self.events_list if e.event_type == 'objectPresent']
-
-    def get_objectpresent_axes(self):
-        """
-        Returns filtered lists of timestamps and state
-        from objectPresent events in history.
-
-        Returns
-        -------
-        timestamp : list[datetime]
-            List of event timestamps.
-        state : list[str]
-            List of whether an object has been PRESENT or NOT_PRESENT.
-
-        """
-
-        x = []
-        y = []
-        for e in self.events_list:
-            if e.event_type == 'objectPresent':
-                x.append(e.data.timestamp)
-                y.append(e.data.state)
-        return x, y
-
-    def get_humidity_events(self):
-        """
-        Returns a filtered list of humidity events from history.
-
-        Returns
-        -------
-        events : list[Event]
-            List of humidity events.
-
-        """
-
-        return [e for e in self.events_list if e.event_type == 'humidity']
-
-    def get_humidity_axes(self):
-        """
-        Returns filtered lists of timestamps, temperature, and humidity values
-        from humidity events in history.
-
-        Returns
-        -------
-        timestamp : list[datetime]
-            List of event timestamps.
-        temperature : list[float]
-            List of temperature values in degrees Celsius.
-        humidity : list[int]
-            List of relative humidities in percent.
-
-        """
-
-        x = []
-        y = []
-        z = []
-        for e in self.events_list:
-            if e.event_type == 'humidity':
-                x.append(e.data.timestamp)
-                y.append(e.data.temperature)
-                y.append(e.data.humidity)
-        return x, y, z
-
-    def get_objectpresentcount_events(self):
-        """
-        Returns a filtered list of objectPresentCount events from history.
-
-        Returns
-        -------
-        events : list[Event]
-            List of objectPresentCount events.
-
-        """
-
-        return [
-            e for e in self.events_list if e.event_type == 'objectPresentCount'
-        ]
-
-    def get_objectpresentcount_axes(self):
-        """
-        Returns filtered lists of timestamps and total counts
-        from objectPresentCount events in history.
-
-        Returns
-        -------
-        timestamp : list[datetime]
-            List of event timestamps.
-        total : list[int]
-            List of the total number of times the sensor has detected
-            the appearance or disappearance of an object over its lifetime.
-
-        """
-
-        x = []
-        y = []
-        for e in self.events_list:
-            if e.event_type == 'objectPresentCount':
-                x.append(e.data.timestamp)
-                y.append(e.data.total)
-        return x, y
-
-    def get_touchcount_events(self):
-        """
-        Returns a filtered list of touchCount events from history.
-
-        Returns
-        -------
-        events : list[Event]
-            List of touchCount events.
-
-        """
-
-        return [e for e in self.events_list if e.event_type == 'touchCount']
-
-    def get_touchcount_axes(self):
-        """
-        Returns filtered lists of timestamps and total counts
-        from touchCount events in history.
-
-        Returns
-        -------
-        timestamp : list[datetime]
-            List of event timestamps.
-        total : list[int]
-            List of the total number of times the sensor
-            has been touched over its lifetime.
-
-        """
-
-        x = []
-        y = []
-        for e in self.events_list:
-            if e.event_type == 'touchCount':
-                x.append(e.data.timestamp)
-                y.append(e.data.total)
-        return x, y
-
-    def get_waterpresent_events(self):
-        """
-        Returns a filtered list of waterPresent events from history.
-
-        Returns
-        -------
-        events : list[Event]
-            List of waterPresent events.
-
-        """
-
-        return [e for e in self.events_list if e.event_type == 'waterPresent']
-
-    def get_waterpresent_axes(self):
-        """
-        Returns filtered lists of timestamps and states
-        from waterPresent events in history.
-
-        Returns
-        -------
-        timestamp : list[datetime]
-            List of event timestamps.
-        state : list[str]
-            A list of whether water has been PRESENT or NOT_PRESENT.
-
-        """
-
-        x = []
-        y = []
-        for e in self.events_list:
-            if e.event_type == 'waterPresent':
-                x.append(e.data.timestamp)
-                y.append(e.data.state)
-        return x, y
-
-    def get_networkstatus_events(self):
-        """
-        Returns a filtered list of networkStatus events from history.
-
-        Returns
-        -------
-        events : list[Event]
-            List of networkStatus events.
-
-        """
-
-        return [e for e in self.events_list if e.event_type == 'networkStatus']
-
-    def get_networkstatus_axes(self):
-        """
-        Returns filtered lists of timestamps, signal strengths, and rssi
-        from networkStatus events in history.
-
-        Returns
-        -------
-        timestamp : list[datetime]
-            List of event timestamps.
-        total : list[int]
-            List of the total number of times the sensor has detected
-            the appearance or disappearance of an object over its lifetime.
-
-        """
-
-        x = []
-        y = []
-        z = []
-        for e in self.events_list:
-            if e.event_type == 'networkStatus':
-                x.append(e.data.timestamp)
-                y.append(e.data.signal_strength)
-                z.append(e.data.rssi)
-        return x, y, z
-
-    def get_batterystatus_events(self):
-        """
-        Returns a filtered list of batteryStatus events from history.
-
-        Returns
-        -------
-        events : list[Event]
-            List of batteryStatus events.
-
-        """
-
-        return [e for e in self.events_list if e.event_type == 'batteryStatus']
-
-    def get_batterystatus_axes(self):
-        """
-        Returns filtered lists of timestamps and percentages
-        from batteryStatus events in history.
-
-        Returns
-        -------
-        timestamp : list[datetime]
-            List of event timestamps.
-        percentage : list[int]
-            List of coarse percentage estimates (0% to 100%) of
-            the remaining battery.
-
-        """
-
-        x = []
-        y = []
-        for e in self.events_list:
-            if e.event_type == 'batteryStatus':
-                x.append(e.data.timestamp)
-                y.append(e.data.percentage)
-        return x, y
+        if y is not None:
+            return x_axis, y_axis
+        else:
+            return x_axis
