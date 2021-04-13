7bb56b4daf9ce57cfdde53b7f445ac0046fff274
Johannes H.Gjeraker <johannes@disruptive-technologies.com>
2021-04-13 18:04:46 +0200

    removed old parse_errors function

--- disruptive/errors.py
+++ disruptive/errors.py
@@ -1,5 +1,3 @@
-import requests
-from disruptive.responses import DTResponse
 
 
 class DTApiError(Exception):
@@ -143,67 +141,3 @@ class UnknownError(DTApiError):
 
     def __init__(self, message):
         super().__init__(message)
-
-
-# Parses the status code, and returns (error, should_retry, retry_after)
-def parse_error(res: DTResponse, retry_count: int):
-    """
-    Evaluates the status code and returns instructions for
-    how to deal with the error in a request.
-
-    Parameters
-    ----------
-    DTResponse : DTResponse
-        A DTResponse object containing body, status_code and a help message.
-    retry_count : int
-        Current retry attempt.
-
-    Returns
-    -------
-    tuple : tuple
-        The error to be raised, a bool whether to retry the request, and
-        an int defining how long to wait before retrying.
-
-    """
-
-    # Check for arleady caught errors.
-    if res.caught_error is not None:
-        if isinstance(res.caught_error, requests.exceptions.ReadTimeout):
-            return ReadTimeout, True, retry_count**2
-        elif isinstance(res.caught_error, requests.exceptions.ConnectionError):
-            return ConnectionError('Failed to establish connection.'), \
-                   False, \
-                   None
-        else:
-            return res.caught_error, False, None
-    else:
-        # Check for API errors.
-        if res.status_code == 200:
-            return None, False, None
-        elif res.status_code == 400:
-            return BadRequest(res.data), False, None
-        elif res.status_code == 401:
-            # The first retry is #1. Therefor, retry_count < 2 will
-            # result in a a single retry attempt.
-            return Unauthorized(res.data), retry_count < 2, None
-        elif res.status_code == 403:
-            return Forbidden(res.data), False, None
-        elif res.status_code == 404:
-            return NotFound(res.data), False, None
-        elif res.status_code == 409:
-            return Conflict(res.data), False, None
-        elif res.status_code == 429:
-            if 'Retry-After' in res.headers:
-                return TooManyRequests(res.data), \
-                       True, \
-                       int(res.headers['Retry-After'])
-            else:
-                return TooManyRequests(res.data), False, None
-        elif res.status_code == 500:
-            return InternalServerError(res.data), True, retry_count**2
-        elif res.status_code == 503:
-            return InternalServerError(res.data), True, retry_count**2
-        elif res.status_code == 504:
-            return InternalServerError(res.data), True, retry_count**2 + 9
-        else:
-            return UnknownError(res.data), False, None
