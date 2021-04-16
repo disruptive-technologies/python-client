3fdf7db8c260da975bb2b70066fae2f9ce6023f4
Johannes H.Gjeraker <johannes@disruptive-technologies.com>
2021-04-16 11:54:22 +0200

    test_create_dataconnector() added

--- tests/test_dataconnector.py
+++ tests/test_dataconnector.py
@@ -84,3 +84,52 @@ class TestDataconnector():
         assert isinstance(dataconnectors, list)
         for d in dataconnectors:
             assert isinstance(d, dt.DataConnector)
+
+    def test_create_dataconnector(self, request_mock):
+        # Update the response json with a mock dataconnector response.
+        res = dtapiresponses.configured_dataconnector
+        request_mock.json = res
+
+        # Call DataConnector.configured_dataconnector().
+        d = dt.DataConnector.create_dataconnector(
+            project_id='c0md3pm0p7bet3vico8g',
+            display_name='my-new-dcon',
+            labels=['name', 'custom-label-01', 'custom_label-02'],
+            config=dt.dataconnector_configs.HttpPush(
+                url='https://584087e0a1fa.eu.ngrok.io/api/endpoint',
+                signature_secret='some-very-good-secret',
+                headers={
+                    'another-header': 'header-contents',
+                    'some-header': 'abc123',
+                }
+            )
+        )
+
+        # Verify expected request outgoing parameters.
+        # Especially the body is important here.
+        url = dt.api_url+'/projects/c0md3pm0p7bet3vico8g/dataconnectors'
+        request_mock.assert_requested(
+            method='POST',
+            url=url,
+            body={
+                'status': 'ACTIVE',
+                'events': [],
+                'labels': ['name', 'custom-label-01', 'custom_label-02'],
+                'displayName':
+                'my-new-dcon',
+                'type': 'HTTP_PUSH',
+                'httpConfig': {
+                    'url': 'https://584087e0a1fa.eu.ngrok.io/api/endpoint',
+                    'signatureSecret': 'some-very-good-secret',
+                    'headers': {
+                        'another-header': 'header-contents',
+                        'some-header': 'abc123'}
+                }
+            }
+        )
+
+        # Assert single request sent.
+        request_mock.assert_request_count(1)
+
+        # Assert output is instance of DataConnector.
+        assert isinstance(d, dt.DataConnector)
