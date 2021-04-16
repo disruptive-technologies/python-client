2eff7d85d4b156ad2c983b55171be328d05b4f17
Johannes H.Gjeraker <johannes@disruptive-technologies.com>
2021-04-16 12:08:34 +0200

    added test_update_dataconnector() and test_update_dataconnector_change_nothing()

--- tests/test_dataconnector.py
+++ tests/test_dataconnector.py
@@ -1,3 +1,6 @@
+# Standard library imports.
+from unittest.mock import patch
+
 # Project imports.
 import disruptive as dt
 import tests.api_responses as dtapiresponses
@@ -115,8 +118,7 @@ class TestDataconnector():
                 'status': 'ACTIVE',
                 'events': [],
                 'labels': ['name', 'custom-label-01', 'custom_label-02'],
-                'displayName':
-                'my-new-dcon',
+                'displayName': 'my-new-dcon',
                 'type': 'HTTP_PUSH',
                 'httpConfig': {
                     'url': 'https://584087e0a1fa.eu.ngrok.io/api/endpoint',
@@ -133,3 +135,81 @@ class TestDataconnector():
 
         # Assert output is instance of DataConnector.
         assert isinstance(d, dt.DataConnector)
+
+    def test_update_dataconnector(self, request_mock):
+        # Update the response json with a mock dataconnector response.
+        res = dtapiresponses.configured_dataconnector
+        request_mock.json = res
+
+        # Call DataConnector.configured_dataconnector().
+        d = dt.DataConnector.update_dataconnector(
+            dataconnector_id='c16pegipidie7lltrefg',
+            project_id='c0md3pm0p7bet3vico8g',
+            display_name='my-new-dcon',
+            labels=['name', 'custom-label-01', 'custom_label-02'],
+            config=dt.dataconnector_configs.HttpPush(
+                url='https://584087e0a1fa.eu.ngrok.io/api/endpoint',
+                signature_secret='some-very-good-secret',
+                headers={
+                    'another-header': 'header-contents',
+                    'some-header': 'abc123',
+                }
+            )
+        )
+
+        # Verify expected request outgoing parameters.
+        # Especially the body is important here.
+        url = dt.api_url
+        url += '/projects/c0md3pm0p7bet3vico8g'
+        url += '/dataconnectors/c16pegipidie7lltrefg'
+        request_mock.assert_requested(
+            method='PATCH',
+            url=url,
+            body={
+                'labels': ['name', 'custom-label-01', 'custom_label-02'],
+                'displayName': 'my-new-dcon',
+                'httpConfig': {
+                    'url': 'https://584087e0a1fa.eu.ngrok.io/api/endpoint',
+                    'signatureSecret': 'some-very-good-secret',
+                    'headers': {
+                        'another-header': 'header-contents',
+                        'some-header': 'abc123',
+                    }
+                }
+            }
+        )
+
+        # Assert single request sent.
+        request_mock.assert_request_count(1)
+
+        # Assert output is instance of DataConnector.
+        assert isinstance(d, dt.DataConnector)
+
+    def test_update_dataconnector_change_nothing(self, request_mock):
+        """
+        Verify that if not provided, optional parameters should not
+        be included in the request body. This is important as we do
+        not want the API to change anything not provided.
+
+        """
+
+        # Mock the DataConnector constructor to do nothing as
+        # the response is not relevant to this test.
+        with patch('disruptive.DataConnector.__init__') as init_mock:
+            # Do nothing and return None.
+            init_mock.return_value = None
+
+            # Call DataConnector.configured_dataconnector() with only
+            # required parameters, basically asking the API to change nothing.
+            dt.DataConnector.update_dataconnector(
+                dataconnector_id='dataconnector_id',
+                project_id='project_id',
+            )
+
+        # Verify that all optional parameters are not included in the body.
+        url = dt.api_url+'/projects/project_id/dataconnectors/dataconnector_id'
+        request_mock.assert_requested(
+            method='PATCH',
+            url=url,
+            body={},
+        )
