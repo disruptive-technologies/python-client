d0c9806350ba4196365f45f580d23802f6803097
Johannes H.Gjeraker <johannes@disruptive-technologies.com>
2021-04-19 13:06:02 +0200

    rewrote __repr__ be more consistent with python typing

--- disruptive/outputs.py
+++ disruptive/outputs.py
@@ -27,16 +27,14 @@ class OutputBase():
         return '\n'.join(out)
 
     def __dump(self, out, obj, level):
-        # Set the two formatting indent levels.
+        # Set the indent level for formatting.
         n_spaces = 4
-        ls = level*' '*n_spaces
+        l0 = level*' '*n_spaces
+        l1 = (level+1)*' '*n_spaces
 
-        # Print name of the current object if level is 0.
+        # At first recursive depth, print object name.
         if level == 0:
-            out.append(ls + str(self.__class__.__name__) + ' object:')
-            # out.insert(-1, ls + '_'*len(out[-1]))
-            out.insert(-1, '')
-            out.append(ls + '-'*len(out[-1]))
+            out.append(l0 + str(obj.__class__.__name__) + '(')
 
         # Append the various public attributes recursively.
         for a in vars(obj):
@@ -46,34 +44,45 @@ class OutputBase():
 
             # Fetch and evaluate the attribute value / type.
             val = getattr(obj, a)
+
+            # Class objects should be dumped recursively.
             if hasattr(val, '__dict__'):
-                # Class objects should be dumped recursively.
-                out.append('{}[{}] {}:'.format(ls, type(val).__name__, a))
+                out.append('{}{}: {} = {}'.format(
+                    l1, a, type(val).__name__,
+                    str(val.__class__.__name__) + '('))
                 self.__dump(out, val, level=level+1)
 
+            # Lists content should be iterated through.
             elif isinstance(val, list):
-                # Lists content should be iterated through.
-                out.append('{}[{}] {}:'.format(ls, type(val).__name__, a))
-                self.__dump_list(out, val, level+1)
+                out.append('{}{}: {} = {}'.format(
+                    l1, a, type(val).__name__, '['))
+                self.__dump_list(out, val, level+1, n_spaces, l1)
+                out.append(l1 + '],')
 
+            # Other types can be printed directly.
             else:
-                # Other types can be printed directly.
-                out.append('{}[{}] {}: {}'.format(
-                    ls, type(val).__name__, a, str(val)
+                out.append('{}{}: {} = {},'.format(
+                    l1, a, type(val).__name__, str(val)
                 ))
+
+        # At the end of each recursive depth, end object paranthesis.
+        out.append(l0 + '),')
 
         return out
 
-    def __dump_list(self, out, lst, level):
-        n_spaces = 4
-        ls = (level+1)*' '*n_spaces
+    def __dump_list(self, out, lst, level, n_spaces, l1):
         for val in lst:
+            # Class objects should be dumped recursively.
             if hasattr(val, '__dict__'):
-                out.append('{}[{}]:'.format(ls, type(val).__name__))
+                out.append('{}{}'.format(
+                    l1, str(val.__class__.__name__) + '('
+                ))
                 self.__dump(out, val, level=level+2)
+
+            # Everything else can be printed directly.
             else:
-                out.append('{}[{}] {}'.format(
-                    ls, type(val).__name__, val
+                out.append('{}{} = {},'.format(
+                    l1, type(val).__name__, str(val)
                 ))
         return out
 
