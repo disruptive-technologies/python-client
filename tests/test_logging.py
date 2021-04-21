4ec5f5687aec99c236e1b786d15bae279838629e
Johannes H.Gjeraker <johannes@disruptive-technologies.com>
2021-04-21 14:11:38 +0200

    added logging tests for log()

---
+++ tests/test_logging.py
@@ -0,0 +1,58 @@
+# Standard library imports.
+from unittest.mock import patch
+from datetime import datetime
+
+# Project imports.
+import disruptive
+import disruptive.logging as dtlog
+
+
+class TestLogging():
+
+    @patch('disruptive.log', True)
+    def test_default_enabled(self):
+        # Verify default flag fisabled.
+        assert disruptive.log is True
+
+        # Do some logging.
+        with patch('builtins.print') as print_mock:
+            dtlog.log('This is a message.')
+
+        # Verify print called only once.
+        assert print_mock.call_count == 1
+
+    @patch('disruptive.log', False)
+    def test_default_disabled(self):
+        # Verify default flag fisabled.
+        assert disruptive.log is False
+
+        # Do some logging.
+        with patch('builtins.print') as print_mock:
+            dtlog.log('This is a message.')
+
+        # Verify print never called.
+        assert print_mock.call_count == 0
+
+    @patch('disruptive.log', False)
+    def test_override_enabled(self):
+        # Verify default flag fisabled.
+        assert disruptive.log is False
+
+        # Do some logging.
+        with patch('builtins.print') as print_mock:
+            dtlog.log('This is a message.', override=True)
+
+        # Verify print called once.
+        assert print_mock.call_count == 1
+
+    @patch('disruptive.log', True)
+    def test_override_disabled(self):
+        # Verify default flag enabled.
+        assert disruptive.log is True
+
+        # Do some logging.
+        with patch('builtins.print') as print_mock:
+            dtlog.log('This is a message.', override=False)
+
+        # Verify print never called.
+        assert print_mock.call_count == 0
