32a9a23724535386e950bc542e5e140dbda2f980
Johannes H.Gjeraker <johannes@disruptive-technologies.com>
2021-04-22 18:19:43 +0200

    removed old types module

--- disruptive/types/__init__.py
+++
@@ -1,3 +0,0 @@
-import disruptive.types.events as events  # noqa
-import disruptive.types.devices as devices  # noqa
-import disruptive.types.roles as roles  # noqa
--- disruptive/types/devices.py
+++
@@ -1,7 +0,0 @@
-temperature = 'temperature'
-proximity = 'proximity'
-touch = 'touch'
-humidity = 'humidity'
-proximity_counter = 'proximityCounter'
-touch_counter = 'touchCounter'
-water_detector = 'waterDetector'
