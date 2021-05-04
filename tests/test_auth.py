c7f3a821d9976a135d705d546aac0fa03f8a9117
Johannes H.Gjeraker <johannes@disruptive-technologies.com>
2021-05-04 10:19:17 +0200

    tests for None and empty string credentials

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,13 +1,23 @@
-# Third-party imports.
 import pytest
 
-# Project imports.
-import disruptive as dt
+import disruptive
+import disruptive.errors as dterrors
 import tests.api_responses as dtapiresponses
 from disruptive.authentication import Auth
 
 
 class TestAuth():
+
+    def test_repr(self, request_mock):
+        # Update the response json with a mock token response.
+        res = dtapiresponses.auth_token_fresh
+        request_mock.json = res
+
+        # Fetch a role.
+        x = disruptive.Auth.serviceaccount('key_id', 'secret', 'email')
+
+        # Evaluate __repr__ function and compare copy.
+        eval(repr(x))
 
     def test_serviceaccount_auth(self, request_mock):
         # Update the response json with a mock token response.
@@ -15,7 +25,7 @@ class TestAuth():
         request_mock.json = res
 
         # Call the two classmethod constructors.
-        auth = dt.Auth.serviceaccount('key_id', 'secret', 'email')
+        auth = disruptive.Auth.serviceaccount('key_id', 'secret', 'email')
 
         # Assert token post request not sent at construction.
         request_mock.assert_request_count(0)
@@ -29,7 +39,7 @@ class TestAuth():
         request_mock.json = res
 
         # Create an authentication object.
-        auth = dt.Auth.serviceaccount('key_id', 'secret', 'email')
+        auth = disruptive.Auth.serviceaccount('key_id', 'secret', 'email')
 
         # Verify expired token.
         assert auth._has_expired()
@@ -44,11 +54,20 @@ class TestAuth():
         # Verify non-expired token.
         assert not auth._has_expired()
 
-    def test_raise_missing_credential(self):
-        # Verify TypeError raised at missing input credential.
-        with pytest.raises(TypeError):
-            dt.Auth.serviceaccount(None, 'secret', 'email')
-        with pytest.raises(TypeError):
-            dt.Auth.serviceaccount('key_id', None, 'email')
-        with pytest.raises(TypeError):
-            dt.Auth.serviceaccount('key_id', 'secret', None)
+    def test_raise_none_credential(self):
+        # Verify TypeError raised at None input credential.
+        with pytest.raises(dterrors.TypeError):
+            disruptive.Auth.serviceaccount(None, 'secret', 'email')
+        with pytest.raises(dterrors.TypeError):
+            disruptive.Auth.serviceaccount('key_id', None, 'email')
+        with pytest.raises(dterrors.TypeError):
+            disruptive.Auth.serviceaccount('key_id', 'secret', None)
+
+    def test_raise_empty_string_credential(self):
+        # Verify EmptyStringError raised at missing input credential.
+        with pytest.raises(dterrors.EmptyStringError):
+            disruptive.Auth.serviceaccount('', 'secret', 'email')
+        with pytest.raises(dterrors.EmptyStringError):
+            disruptive.Auth.serviceaccount('key_id', '', 'email')
+        with pytest.raises(dterrors.EmptyStringError):
+            disruptive.Auth.serviceaccount('key_id', 'secret', '')
