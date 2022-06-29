0078be5d3d2e123985fa7f8c3ef75fd1dd35b415
Johannes H.Gjeraker <59526817+jhgjeraker@users.noreply.github.com>
2022-06-29 09:17:10 +0200

    feat: credentials file authentication (#110)

--- disruptive/__init__.py
+++ disruptive/__init__.py
@@ -1,11 +1,25 @@
 # Metadata
 __version__ = '1.4.1'
 
+# If set, logs of chosen level and higher are printed to console.
+# Available levels are: debug, info, warning, error, critical.
+log_level = None
+
+# REST API base URLs of which all endpoints are an expansion.
+base_url = 'https://api.disruptive-technologies.com/v2'
+emulator_base_url = 'https://emulator.disruptive-technologies.com/v2'
+
+# If a request response contains an error for which a series of retries is
+# worth considering, these variable determine how long to wait without an
+# answer, and how many times the package should retry before raising an error.
+request_timeout = 3  # seconds
+request_attempts = 3  # attempts
+
 # Authentication scheme.
 from disruptive.authentication import Auth as Auth  # noqa
 
 # Initialize package with environment variables authentication scheme.
-default_auth = Auth._service_account_env_vars()
+default_auth = Auth.init()
 
 # Resources.
 from disruptive.resources.device import Device as Device  # noqa
@@ -22,17 +36,3 @@ from disruptive.resources.emulator import Emulator as Emulator  # noqa
 from disruptive import events as events  # noqa
 from disruptive import outputs as outputs  # noqa
 from disruptive import errors as errors  # noqa
-
-# If set, logs of chosen level and higher are printed to console.
-# Available levels are: debug, info, warning, error, critical.
-log_level = None
-
-# REST API base URLs of which all endpoints are an expansion.
-base_url = 'https://api.disruptive-technologies.com/v2'
-emulator_base_url = 'https://emulator.disruptive-technologies.com/v2'
-
-# If a request response contains an error for which a series of retries is
-# worth considering, these variable determine how long to wait without an
-# answer, and how many times the package should retry before raising an error.
-request_timeout = 3  # seconds
-request_attempts = 3  # attempts
